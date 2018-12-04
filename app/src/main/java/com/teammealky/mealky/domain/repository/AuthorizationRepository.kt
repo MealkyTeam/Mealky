37bcb0e6438c983f4f62a15df64d8bdc4523865a
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2018-12-04 08:21:48 +0100

    Added saving username on login with password.

--- app/src/main/java/com/teammealky/mealky/domain/repository/AuthorizationRepository.kt
+++ app/src/main/java/com/teammealky/mealky/domain/repository/AuthorizationRepository.kt
@@ -1,14 +1,12 @@
 package com.teammealky.mealky.domain.repository
 
-
-import com.teammealky.mealky.domain.model.Token
 import com.teammealky.mealky.domain.model.User
 import io.reactivex.Single
 import retrofit2.Response
 
 interface AuthorizationRepository {
     fun signInWithPassword(email: String,
-                           password: String): Single<Token>
+                           password: String): Single<User>
 
     fun signInWithToken(token: String): Single<User>
 
