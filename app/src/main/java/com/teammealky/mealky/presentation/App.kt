22964131033e10a7b3901ffec6311919e98cc1b1
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2019-01-20 13:54:43 +0100

    integrated (#25)

--- app/src/main/java/com/teammealky/mealky/presentation/App.kt
+++ app/src/main/java/com/teammealky/mealky/presentation/App.kt
@@ -2,16 +2,40 @@ package com.teammealky.mealky.presentation
 
 import android.app.Application
 import android.content.Context
+import android.os.StrictMode
+import com.crashlytics.android.Crashlytics
+import com.teammealky.mealky.BuildConfig
 import com.teammealky.mealky.presentation.commons.injection.component.ApplicationComponent
 import com.teammealky.mealky.presentation.commons.injection.component.DaggerApplicationComponent
 import com.teammealky.mealky.presentation.commons.injection.module.ApplicationModule
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+
+        Fabric.with(this, Crashlytics())
+        if (BuildConfig.DEBUG) {
+            try {
+                Timber.plant(Timber.DebugTree())
+                StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                        .detectDiskReads()
+                        .detectDiskWrites()
+                        .detectNetwork()
+                        .penaltyLog()
+                        .build())
+
+                StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                        .detectLeakedSqlLiteObjects()
+                        .detectLeakedClosableObjects()
+                        .penaltyLog()
+                        .build())
+
+            } catch (ignored: Exception) {
+            }
+        }
     }
 
     companion object {
